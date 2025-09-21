f2e78b5cfd8a80c13ebefe43e87015e8afab7a8d
elliot <eday34@gatech.edu>
2025-09-21 13:56:38 -0400

    Page setup

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    template_data = {
+        'title': 'Job Site - Home'
+    }
+    return render(request, 'home/index.html', {'template_data': template_data})

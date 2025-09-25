e3e45775db0776c4d19e6950c3a8680325fcd770
elliot <eday34@gatech.edu>
2025-09-25 16:12:13 -0400

    Changed website name ot Jobify

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    template_data = {"title": "Job Site - Home"}
+    template_data = {"title": "Jobify - Home"}
     return render(request, "home/index.html", {"template_data": template_data})
